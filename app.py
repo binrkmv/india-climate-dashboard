41db24566f11d7ce5d7ba7b56a1356640b8fc31d
Binay Shankar <88761951+binrkmv@users.noreply.github.com>
2026-06-20 17:47:47 +0530

    app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ with right_panel_column:
     st.markdown("### ℹ️ Dashboard Info")
     st.markdown(
         """
-        **Created by:** [Binay Shankar](binayshankar.weebly.com)
+        * **Created by:** [Binay Shankar](binayshankar.weebly.com)
         
         **Data Sources:**
         * **Shapefile (Census 2001):** [Census of India](https://onlinemaps.surveyofindia.gov.in/)  
